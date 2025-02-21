3f0430868b28e4635729206fd1e15e1f978823be
arata-haruto <138739158+arata-haruto@users.noreply.github.com>
2025-02-21 15:44:19 +0900

    ランキング追加1

--- teamC/Scenes/Ranking/RankingScene.cpp
+++ teamC/Scenes/Ranking/RankingScene.cpp
@@ -1,1 +1,62 @@
 #include "RankingScene.h"
+#include "../../Utility/InputManager.h"
+#include "../../Utility/ResourceManager.h"
+#include "DxLib.h"
+
+RankingScene::RankingScene()
+{
+
+}
+
+RankingScene::~RankingScene()
+{
+
+}
+
+void RankingScene::Initialize()
+{
+	
+}
+
+eSceneType RankingScene::Update(float delta_second)
+{
+	// 入力情報を取得
+	InputManager* input = InputManager::GetInstance();
+
+	// タイトルシーンに遷移する
+	if (input->GetKeyState(KEY_INPUT_SPACE))
+	{
+		return eSceneType::eTitle;
+	}
+	if (input->GetButtonState(XINPUT_BUTTON_START))
+	{
+		return eSceneType::eTitle;
+	}
+
+	// 親クラスの更新処理を呼び出す
+	return __super::Update(delta_second);
+}
+
+void RankingScene::Draw() const
+{
+	// 親クラスの描画処理を呼び出す
+	//__super::Draw();
+
+	DrawFormatString(50, 50, GetColor(255, 255, 255), "ランキングです");
+
+	// UIの描画
+	//DrawRotaGraph(320, 240, 1.0, 0.0, title_image, TRUE);
+	DrawFormatString(10, 640, GetColor(255, 0, 0), "Space key pressed back title");
+	SetFontSize(16);
+}
+
+void RankingScene::Finalize()
+{
+	// 親クラスの終了時処理を呼び出す
+	__super::Finalize();
+}
+
+eSceneType RankingScene::GetNowSceneType() const
+{
+	return eSceneType::eRanking;
+}
