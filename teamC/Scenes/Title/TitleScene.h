e6703138b34b6c991e157f68b0877f2cde4b4fc4
Raimu0319 <raimu-nakayama-k23h@stu.kbc.ac.jp>
2025-02-24 10:41:55 +0900

    Titleの作成とHelpSceneに処理の追加しました。
    ESCキーを押すと終了できるようにした。

--- teamC/Scenes/Title/TitleScene.h
+++ teamC/Scenes/Title/TitleScene.h
@@ -1,6 +1,14 @@
 #pragma once
 #include "../SceneBase.h"
 
+enum eSelectMenu
+{
+	eNONE,
+	ePLAY,
+	eHELP,
+	eEXIT
+};
+
 class TitleScene : public SceneBase
 {
 public:
@@ -8,14 +16,18 @@ public:
 	std::vector<int> UI_string;
 	std::vector<int> UI_coin;
 
+	int pickup_color;
+	int dropoff_color;
 	int num_image;
 	int string_image[26];
-	int coin_image;
+	eSelectMenu select_menu;
 
 	virtual void Initialize() override;
 	virtual eSceneType Update(float delta_second) override;
 	virtual void Draw() const override;
 	virtual void Finalize() override;
 	virtual eSceneType GetNowSceneType() const override;
+	void SetUpSelectMenuType();
+	void SetDownSelectMenuType();
 };
 
