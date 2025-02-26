41ea23584914cab559eaf83957e7cb86a5f74fe5
NakamaSyouta <138636023+NakamaSyouta@users.noreply.github.com>
2025-02-26 12:21:53 +0900

    sord_追加

---
+++ teamC/Objects/Effect/Sord.cpp
@@ -0,0 +1,45 @@
+#include "Sord.h"
+#include "../../Objects/GameObject.h"
+#include"../../Utility/InputManager.h"
+#include "../../Utility/ResourceManager.h"
+#include "../../Utility/ScoreManager.h"
+#include"../../Utility/Application.h"
+#include"DxLib.h"
+Sord::Sord()
+{
+}
+
+Sord::~Sord()
+{
+}
+
+void Sord::Initialize()
+{
+	//•Ší‚Ì‰æ‘œ‚Ì“Ç‚İ‚İ
+	ResourceManager* rm = ResourceManager::GetInstance();
+	image = rm->GetImageResource("Resource/item/Enemy_sword.png")[0];
+
+	if (image == -1)
+	{
+		// ‰æ‘œ“Ç‚İ‚İ¸”s‚Ìê‡‚Ìˆ—
+		throw ("Resource/item/Enemy_sword.png‚ª“Ç‚İ‚ß‚Ü‚¹‚ñ‚Å‚µ‚½\n");
+
+	}
+}
+
+void Sord::Update(float delta_second)
+{
+}
+
+void Sord::Draw(const Vector2D& screen_offset) const
+{
+	__super::Draw(screen_offset);
+}
+
+void Sord::Finalize()
+{
+}
+
+void Sord::OnHitCollision(GameObject* target)
+{
+}
--- teamC/Objects/Effect/Sord.h
+++ teamC/Objects/Effect/Sord.h
@@ -1,1 +1,36 @@
 #pragma once
+#include"../../Objects/GameObject.h"
+
+class Sord:public GameObject
+{
+private:
+	std::vector<int> item_image;		// 武器画像
+public:
+	Sord();
+	~Sord();
+
+
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
+	virtual void Initialize();
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	/// <param name="delta_second">1フレームあたりの時間</param>
+	virtual void Update(float delta_second);
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	/// <param name="screen_offset">オフセット値</param>
+	virtual void Draw(const Vector2D& screen_offset) const;
+	/// <summary>
+	/// 終了時処理
+	/// </summary>
+	virtual void Finalize();
+
+public:
+	virtual void OnHitCollision(GameObject* target) override;
+private:
+
+};
